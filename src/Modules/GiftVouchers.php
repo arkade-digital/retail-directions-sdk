24314574ded63eaf3b5f4ec61a4c32f7e282e652
Andy Johnston <andy@arkade.com.au>
2018-10-26 17:06:05 +1100

    gift voucher 2 step

--- src/Modules/GiftVouchers.php
+++ src/Modules/GiftVouchers.php
@@ -6,6 +6,7 @@ use DomainException;
 use Illuminate\Support\Collection;
 use Arkade\RetailDirections\GiftVoucher;
 use Arkade\RetailDirections\Exceptions;
+use Arkade\RetailDirections\PaymentDetail;
 
 class GiftVouchers extends AbstractModule
 {
@@ -44,26 +45,37 @@ class GiftVouchers extends AbstractModule
 	 */
     public function issueGiftVoucher($payload)
     {
+        /*
+         * Single Step Sample
+         * $payload = [
+                'giftvoucherscheme_code' => '000487h1h',
+                'store_code' => '1112',
+                'issued_currency_code' => 'AUD',
+                'amount' => 50,
+                'reference_code' => 'abcdefghijklinn',//this is payment reference number
+                'purchaser_first_name' => 'tejas',
+                'recipient_first_name' => 'tejas',
+                'recipient_email_address' => 'tejas@arkade.com.au',
+                'message' => 'hellow how are you',
+                'fulfilment_method_ind' => 'V',//V for virtual P for physical
+                'status_ind' => 'A'
+        * ];
+        * Multi Step Sample
+        * $payload = [
+                'giftvoucherscheme_code' => '000487h1h',
+                'store_code' => '1112',
+                'issued_currency_code' => 'AUD',
+                'amount' => 50,
+                'reference_code' => 'abcdefghijklinn',//this is payment reference number
+                'purchaser_first_name' => 'tejas',
+                'recipient_first_name' => 'tejas',
+                'recipient_email_address' => 'tejas@arkade.com.au',
+                'message' => 'hellow how are you',
+                'fulfilment_method_ind' => 'V',//V for virtual P for physical
+                'status_ind' => 'P'
+        * ];
+        */
         try {
-            /*
-             * Working example
-             * $response = $this->client->call('DoGiftVoucherRequest', [
-                'VoucherRequest' => [
-                    'giftvoucherscheme_code' => '000487h1h',
-                    'store_code' => '1112',
-                    'issued_currency_code' => 'AUD',
-                    'amount' => 50,
-                    'reference_code' => 'abcdefghijklinn',//this is payment reference number
-                    'purchaser_first_name' => 'tejas',
-                    'recipient_first_name' => 'tejas',
-                    'recipient_email_address' => 'tejas@arkade.com.au',
-                    'message' => 'hellow how are you',
-                    'fulfilment_method_ind' => 'V',//V for virtual P for physical
-                    'status_ind' => 'A'
-                ],
-            ],
-                'VoucherRequest'
-            );*/
             $response = $this->client->call('DoGiftVoucherRequest', [
                 'VoucherRequest' => $payload,
             ],
@@ -77,6 +89,56 @@ class GiftVouchers extends AbstractModule
             }
             throw $e;
         }
+
+        return $response->VoucherRequest;
+    }
+
+    /**
+     * Finalise pending giftcard.
+     *
+     * @param  array $payload
+     * @param PaymentDetail[]|Collection $payments
+     *
+     * @return  \SimpleXMLElement
+     * @throws Exceptions\ServiceException
+     */
+    public function finaliseGiftVoucher($payload, $payments)
+    {
+        /*
+        * Working example
+        * $payload = [
+               'giftvoucherrequest_code' => '000487h1h',
+               'store_code' => '1112',
+               'status_ind' => 'A'
+               'finalise_user' => 'WEB',
+               'finalise_date_time' => '2013-09-02T09:58:57.6167226+10:00',
+               'generate_giftcard_ind' => 'N'
+               'fulfilment_method_ind' => 'V',//V for virtual P for physical
+           ];
+        */
+        $payload = [
+            'VoucherRequestFinalise' => $payload
+        ];
+
+        // Add payments to order is available, this is the last chance below the order is locked
+        if ($payments->count()) {
+            $payload['PaymentDetails'] = $payments->map(function(PaymentDetail $payment) {
+                return $payment->getXmlArray();
+            })->toArray();
+        }
+
+        try {
+            $response = $this->client->call('DoGiftVoucherRequestFinalise', $payload, 'VoucherRequestFinalise');
+        } catch (Exceptions\ServiceException $e) {
+
+            if (60103 == $e->getCode()) {
+                throw (new Exceptions\NotFoundException)
+                    ->setHistoryContainer($e->getHistoryContainer());
+            }
+            throw $e;
+        }
+
+        dump($response);
 
         return $response->VoucherRequest;
     }
